1d5606813a30423a1bff2843e2d0b20abf8435cf
Agape D'sky <ikanblackghost@gmail.com>
2022-11-09 19:43:00 +0700

    Added git ignore, update params.cpp comment

--- params.cpp
+++ params.cpp
@@ -1,3 +1,6 @@
 #include "Include/params.h"
 
+// Please configure count_val before changing demon_endgoal_loc content
+// The last value of the array is the endgoal location
+// ex. : {1,2,3,4} -> 1,2,3 contain demons, 4 is the endgoal
 int demon_endgoal_loc[count_val] = {2,4,5};
